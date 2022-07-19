0772eac5610fdbda588800cb2b9bd4753812b95d
Rohan <rrohan419@gmail.com>
2022-07-19 09:02:39 +0530

    Ecommerce Front End Initial push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { CartServiceService } from './service/cart-service.service';
+import { Product } from './service/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'assign3';
+
+  cartData: any = [];
+  lastSavedQuantity: any;
+  cartTotal: any;
+  list = this.cartService.productList.asObservable();
+  total = this.cartService.cartQuantity.asObservable();
+  myCartTotalPrice = this.cartService.totalPrice.asObservable();
+
+  productDiscountedPrice=0;
+
+  constructor(private cartService: CartServiceService) {
+    // cartService.getLocalStorageSavedProducts();
+  }
+  
+
+  ngOnInit(): void {
+    this.total.subscribe((data: number) => {
+      this.lastSavedQuantity = data;
+      console.log(this.lastSavedQuantity, "hamas");
+    });
+
+    this.list.subscribe((data: any) => {
+      this.cartData = data;
+      console.log(this.cartData);
+    });
+
+    this.myCartTotalPrice.subscribe((data) => {
+      this.cartTotal = data;
+      this.productDiscountedPrice = data
+      console.log(this.cartTotal, "cart total");
+    })
+  }
+  getAllCartProducts() {
+    this.list.subscribe((data) => {
+      this.cartData = data;
+    });
+  }
+
+  clearAllProducts() {
+    this.cartService.clearCart();
+    this.cartTotal = 0;
+    this.cartData = [];
+    this.cartService.myCartTotalPrice = JSON.parse('0');
+    this.cartService.cartCount = 0;
+    this.cartService.cartItem = [];
+    localStorage.setItem('cartQuantity', JSON.stringify(0));
+    localStorage.setItem('savedItems', JSON.stringify([]));
+    localStorage.setItem('cartPrice', JSON.stringify(0));
+    this.lastSavedQuantity = localStorage.getItem('cartQuantity');
+  }
+  deleteSingleProduct(itemName: any) {
+
+    this.cartService.removeProduct(itemName);
+    return this.getAllCartProducts();
+  }
+
 }
---
+++ src/app/service/cart-service.service.ts
@@ -0,0 +1,153 @@
+import { Injectable, OnInit } from '@angular/core';
+import { AsyncSubject, BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
+import swal from 'sweetalert';
+import { Coupons, Product } from './product';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartServiceService implements OnInit {
+  productList = new BehaviorSubject<any>([]);
+  list = this.productList.asObservable();
+  cartItem: Product[] = [];
+  cartQuantity = new BehaviorSubject<number>(0);
+  total = this.cartQuantity.asObservable();
+  cartCount: any = 0;
+  totalPrice = new BehaviorSubject<number>(0);
+  myCartTotalPrice = this.totalPrice.asObservable()
+  cartTotalPrice: any = 0;
+
+
+
+
+
+  lastSavedData: any;
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
+  getLocalStorageSavedProducts() {
+
+    this.lastSavedData = localStorage.getItem('savedItems');
+    this.cartCount = localStorage.getItem('cartQuantity');
+    this.cartTotalPrice = localStorage.getItem('cartPrice');
+  }
+
+
+  getSavedProducts() {
+    this.list.subscribe((data) => {
+      this.lastSavedData = data;
+    });
+
+    return this.lastSavedData;
+  }
+  getTotalCartPrice() {
+    this.myCartTotalPrice.subscribe((data) => {
+      this.cartTotalPrice = data;
+    })
+    return this.cartTotalPrice;
+  }
+
+  isDuplicate(item: Product) {
+    for (var i = 0; i < this.cartItem.length; i++) {
+      if (item.id === this.cartItem[i].id) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  addProduct(item: Product) {
+ 
+    for (let i = 0; i < this.cartItem.length; i++) {
+      if (item.name === this.cartItem[i].name) {
+        this.cartCount = this.cartCount + 1;
+        this.cartItem[i].quantity = (this.cartItem[i].quantity + 1);
+        this.cartItem[i].totalPrice = (this.cartItem[i].quantity * JSON.parse(this.cartItem[i].price));
+        this.cartTotalPrice = this.cartTotalPrice + JSON.parse(this.cartItem[i].price);
+        this.totalPrice.next(this.cartTotalPrice);
+        this.cartQuantity.next(this.cartCount);
+        this.productList.next(this.cartItem);
+        localStorage.setItem('cartQuantity', JSON.stringify(this.cartCount));
+        localStorage.setItem('savedItems', JSON.stringify(this.cartItem));
+        localStorage.setItem('cartPrice', JSON.stringify(this.cartTotalPrice));
+
+        //adding duplicate products
+
+        // localStorage.setItem('savedItems', JSON.stringify(this.cartItem));
+        // this.cartItem[this.cartItem.length] = item;
+        // this.productList.next(this.cartItem);
+        return;
+      }
+    }
+    item.quantity = 0;
+    item.quantity = item.quantity + 1;
+    item.totalPrice = item.totalPrice + JSON.parse(item.price);
+    this.cartTotalPrice = this.cartTotalPrice + JSON.parse(item.price);
+    this.cartItem[this.cartItem.length] = item;
+    this.cartCount = this.cartCount + 1;
+    this.totalPrice.next(this.cartTotalPrice);
+    this.cartQuantity.next(this.cartCount);
+    this.productList.next(this.cartItem);
+    localStorage.setItem('cartPrice', JSON.stringify(this.cartTotalPrice));
+    localStorage.setItem('savedItems', JSON.stringify(this.cartItem));
+    localStorage.setItem('cartQuantity', JSON.stringify(this.cartCount));
+
+
+    swal({
+      icon: "success",
+      text: "Added"
+    });
+    return;
+
+  }
+
+  removeProduct(itemName: any) {
+    for (var i = 0; i < this.cartItem.length; i++) {
+      if (itemName === this.cartItem[i].name) {
+        if (this.cartItem[i].quantity === 1) {
+          this.cartCount = (this.cartCount - 1);
+          this.cartItem[i].quantity = 0;
+          this.cartItem[i].totalPrice = (this.cartItem[i].totalPrice - JSON.parse(this.cartItem[i].price));
+          this.cartTotalPrice = this.cartTotalPrice - JSON.parse(this.cartItem[i].price);
+          this.totalPrice.next(this.cartTotalPrice);
+          this.cartItem.splice(i, 1);
+          this.cartQuantity.next(this.cartCount);
+          this.productList.next(this.cartItem);
+          localStorage.setItem('savedItems', JSON.stringify(this.cartItem));
+          localStorage.setItem('cartQuantity', JSON.stringify(this.cartCount));
+          localStorage.setItem('cartPrice', JSON.stringify(this.cartTotalPrice));
+          swal({
+            icon: "success",
+            text: "deleted"
+          });
+          return this.getSavedProducts();
+        }
+        this.cartCount = (this.cartCount - 1);
+        this.cartItem[i].quantity = this.cartItem[i].quantity - 1;;
+        this.cartItem[i].totalPrice = (this.cartItem[i].totalPrice - JSON.parse(this.cartItem[i].price));
+        this.cartTotalPrice = this.cartTotalPrice - JSON.parse(this.cartItem[i].price);
+        this.totalPrice.next(this.cartTotalPrice);
+        this.cartQuantity.next(this.cartCount);
+        this.productList.next(this.cartItem);
+        localStorage.setItem('cartPrice', JSON.stringify(this.cartTotalPrice));
+        localStorage.setItem('savedItems', JSON.stringify(this.list));
+        localStorage.setItem('cartQuantity', JSON.stringify(this.total));
+        swal({
+          icon: "success",
+          text: "deleted"
+        });
+        return this.getSavedProducts();
+      }
+    }
+    return;
+  }
+
+  clearCart() {
+    localStorage.clear();
+    return true;
+  }
+}
